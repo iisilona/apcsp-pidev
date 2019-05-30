60959f313f412d275102fa64de7c4b1ed33215c5
Ilona <50212229+iisilona@users.noreply.github.com>
2019-05-30 09:54:23 -0700

    fixed

--- cdemo/areaofcircle2.c
+++ cdemo/areaofcircle2.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 float areaofcircle(float radius);
 
-int main(int argc, char* argv[])
+int main(int ctinput, char* inputstring[2])
 {
   int SIZE = 200;
   char input[SIZE];
   float radstart;
   float radend;
-
-
-while (1)
-{
-  printf("Enter start value:\n");
-  fgets(input, SIZE, stdin);
-  if(sscanf(input, "%f", &radstart) == 1) break;
+  int noinput = 1;
+  
+if (noinput < ctinput)
+ {
+   while (1)
+   {
+     if(sscanf(inputstring[1], "%f", &radstart) == 1) break;
+     printf("That's not a real number, try executing again.\n");
+     exit(0);
+   }
+   while (1)
+   {
+     if(sscanf(inputstring[2], "%f", &radend) == 1) break;
+     printf("That's not a real number, try executing again.\n");
+     exit(0);
+   }
+ }
+if (noinput == ctinput)
+ {
+ while (1)
+   { 
+    printf("Enter start value:\n");
+    fgets(input, SIZE, stdin);
+    if(sscanf(input, "%f", &radstart) == 1) break;
     printf("That's not a real number.\n");
-}
+   }
 
-while (1)
-{
- printf("Enter end value:\n");
-  fgets(input, SIZE, stdin);
-  if (sscanf(input, "%f", &radend) ==1) break;
-    printf("That's not a real number.\n");
-}
+ while (1)
+   {
+   printf("Enter end value:\n");
+   fgets(input, SIZE, stdin);
+   if (sscanf(input, "%f", &radend) == 1) break;
+   printf("That's not a real number.\n");
+   }
+ }
 
   for (float i = radstart; i <= radend; i++)
   {
